e75c6d4f0ecfa271d30b3d887a172acae50f6408
SonHanSol1 <gksthf7869@naver.com>
2022-01-10 14:43:46 +0900

    Initial Commit

---
+++ StudyAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudyAPP
+//
+//  Created by 손한솔 on 2022/01/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
