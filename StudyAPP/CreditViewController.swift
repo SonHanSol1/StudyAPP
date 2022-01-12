de14899fb0b73edec9ae359039890872d16eb214
SonHanSol1 <gksthf7869@naver.com>
2022-01-12 15:11:35 +0900

    finish

---
+++ StudyAPP/CreditViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CreditViewController.swift
+//  StudyAPP
+//
+//  Created by 손한솔 on 2022/01/12.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+
+    @IBAction func touchUpDismissButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil) // present 메소드의 반대되는 메소드
+    }
+}
